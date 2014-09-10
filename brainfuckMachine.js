6876a6ffef824a4665cff9bb5e07b3f0012e5e82
Izzet <izzetStMr@gmail.com>
2014-09-10 23:36:53 +0200

    Přídány podmínky

--- brainfuckMachine.js
+++ brainfuckMachine.js
@@ -4,8 +4,11 @@ function BrainfuckMachine (code, renderer){
 	
 	this.code = code === undefined ? "" : code;
 	this.cursor = 0;
+	this.parStack = [];
+	this.parNum = 0;
 	
 	this.steps = 0;
+	this.maxSteps = 1000;
 	
 	this.renderer = new BrainfuckRenderer();
 	
@@ -30,6 +33,33 @@ function BrainfuckMachine (code, renderer){
 		"." : function (){
 			_this.renderer.add(_this.memory[_this.index]);
 		},
+		"[" : function (){
+			if(_this.memory[_this.index])
+				_this.parStack.push(_this.cursor);
+			else{
+				while(_this.code[_this.cursor] != "]" || _this.parNum){
+					if(_this.code[_this.cursor] == "]")
+						_this.parNum--;
+					_this.cursor++;
+					if(_this.cursor >= _this.code.length){
+						console.warn("Syntax error - unended cycle!");
+						break;
+					}
+					if(_this.code[_this.cursor] == "[")
+						_this.parNum++;
+					
+				};
+				_this.parNum = 0;
+			}
+		},
+		"]" : function (){
+			if(_this.parStack[_this.parStack.length-1] === undefined){
+				console.warn("Syntax error, unstarted cycle!");
+				return;
+			}
+			_this.cursor = _this.parStack[_this.parStack.length-1]-1;
+			_this.parStack.splice(_this.parStack.length-1,1);
+		},
 	};
 	
 };
@@ -42,7 +72,8 @@ BrainfuckMachine.prototype.step = function (){
 	this.renderer.last = this.memory[this.index];
 };
 BrainfuckMachine.prototype.doAll = function (){
-	while(this.cursor < this.code.length && this.steps < 100){
+	this.steps = 0;
+	while(this.cursor < this.code.length && this.steps < this.maxSteps){
 		this.step();
 	};
 };
